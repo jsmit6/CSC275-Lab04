fab8aa2b85e9dd5d829c041b8c60f6a730c1d114
Smith, Joshua L <jsmit6@uis.edu>
2023-10-05 10:54:58 -0500

    Initial Problem 2 Creation

---
+++ Problem2.java
@@ -0,0 +1,15 @@
+	/*
+	 * Problem 2
+	 * 
+	 * This method will read in a file. This file will contain only one line. This line will contain multiple numbers separated by a comma.
+	 * 
+	 * Your method should read each number of this file, and return the sum of all  numbers included.
+	 * 
+	 * For this problem you can assume only integers and commas will be included in any file tested.
+	 * 
+	 * Test File: Problem2.txt
+	 */
+	public int Problem2(File file) {
+
+		return 0;
+	}
