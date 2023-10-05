f754c5634b41851d9fd79388eb3fc5707042ca53
Smith, Joshua L <jsmit6@uis.edu>
2023-10-05 10:53:38 -0500

    Initial Problem Creation

---
+++ Problem01.java
@@ -0,0 +1,14 @@
+	/*
+	 * Problem 1
+	 * 
+	 * This method should read in a given file and return the sum of all of the numbers within the file. Each number will be on it's own individual line, and all lines will have a number.
+	 * 
+	 * For this problem you can assume only integers will be included in any file.
+	 * 
+	 * Test File: Problem1.txt
+	 */
+
+	public int Problem1(File file) {
+
+		return 0;
+	}
